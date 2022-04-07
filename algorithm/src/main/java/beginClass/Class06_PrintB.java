cfe0220ed4cb057072e0e5693b6c3c4f254533ea
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-04-07 20:25:01 +0800

    2022.04.07：learning

---
+++ algorithm/src/main/java/beginClass/Class06_PrintB.java
@@ -0,0 +1,23 @@
+package beginClass;
+
+/**
+ * 将数字的二进制 32 位打印出来
+ *
+ * @author: thirteenmj
+ * @date: 2022-04-07 19:52
+ */
+public class Class06_PrintB {
+
+    public static void print(int number) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print(((number & (1 << i)) == 0 ? 0 : 1));
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int test = 1;
+        print(test);
+    }
+
+}
