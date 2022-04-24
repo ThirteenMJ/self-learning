bf275390dba645297f9323ea8520c9d4b91569a9
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-04-24 15:04:16 +0800

    2022.04.24：learning

--- algorithm/src/main/java/beginClass/class04/Code01_ReverseList.java
+++ algorithm/src/main/java/beginClass/class04/Code01_ReverseList.java
@@ -1,7 +1,6 @@
 package beginClass.class04;
 
 import java.util.ArrayList;
-import java.util.SimpleTimeZone;
 
 /**
  * 给一个链表，要求返回顺序倒过来的值
@@ -59,7 +58,16 @@ public class Code01_ReverseList {
     }
 
     private static DoubleNode revertDoubleList(DoubleNode head) {
-        return null;
+        DoubleNode pre = null;
+        DoubleNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.pre = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
     }
 
     private static Node testReverseLinkedList(Node head) {
@@ -92,14 +100,15 @@ public class Code01_ReverseList {
             list.add(head);
             head = head.next;
         }
-        list.get(0).next = null;
-        DoubleNode pre = list.get(0);
+        DoubleNode pre = null;
+        DoubleNode next = null;
         int length = list.size();
-        for (int i = 1; i < length -1; i++) {
+        for (int i = 0; i < length; i++) {
             DoubleNode cur = list.get(i);
-            cur.next = list.get(i - 1);
-            cur.pre = pre;
-            pre = cur;
+            pre = cur.pre;
+            next = cur.next;
+            cur.pre = next;
+            cur.next = pre;
         }
         return list.get(length - 1);
     }
@@ -108,10 +117,22 @@ public class Code01_ReverseList {
     private static DoubleNode generateDoubleList(int maxValue, int maxLength) {
         int length = (int) (maxLength * Math.random() + 1);
 
-        DoubleNode head = new DoubleNode(1);
+        if (length > 0) {
+            DoubleNode head = new DoubleNode((int) (maxValue * Math.random() + 1));
+            DoubleNode pre = null;
+            DoubleNode next = head;
 
-
-        return head;
+            for (int i = 1; i < length; i++) {
+                DoubleNode node = new DoubleNode((int) (maxValue * Math.random() + 1));
+                node.pre = next;
+                next.next = node;
+                next.pre = pre;
+                pre = next;
+                next = next.next;
+            }
+            return head;
+        }
+        return null;
     }
 
 
@@ -138,6 +159,17 @@ public class Code01_ReverseList {
         }
         System.out.println("单链条测试通过");
         for (int i = 0; i < testTime; i++) {
+            DoubleNode head = generateDoubleList(maxValue, maxLength);
+            DoubleNode result1 = revertDoubleList(head);
+            DoubleNode result2 = testReverseDoubleList(result1);
+            if (!isEqualsForDoubleList(head, result2)) {
+                System.out.println("双链表出错");
+                System.out.println("原双链条：");
+                printDoubleList(head);
+                System.out.println("现双链条：");
+                printDoubleList(result2);
+                return;
+            }
 
         }
 
@@ -145,6 +177,13 @@ public class Code01_ReverseList {
 
     }
 
+    /**
+     * 判断单链表是否相同
+     *
+     * @param firstNode
+     * @param secondNode
+     * @return
+     */
     private static boolean isEqualsForLinkedList(Node firstNode, Node secondNode) {
         while (firstNode != null && secondNode != null) {
             if (firstNode.value != secondNode.value) {
@@ -154,6 +193,58 @@ public class Code01_ReverseList {
             secondNode = secondNode.next;
         }
         return null == firstNode && null == secondNode;
+    }
+
+    /**
+     * 判断双链表是否相同
+     *
+     * @param node1
+     * @param node2
+     * @return
+     */
+    private static boolean isEqualsForDoubleList(DoubleNode node1, DoubleNode node2) {
+        boolean isNull1 = node1 == null;
+        boolean isNull2 = node2 == null;
+        if (isNull1 && isNull1) {
+            return true;
+        }
+        if (isNull1 || isNull2) {
+            return false;
+        }
+
+        if (node1.pre != null || node2.pre != null) {
+            return false;
+        }
+
+        DoubleNode end1 = null;
+        DoubleNode end2 = null;
+
+        while (node1 != null && node2 != null) {
+            if (node1.value != node2.value) {
+                return false;
+            }
+
+            end1 = node1;
+            end2 = node2;
+
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+
+        if (node1 != null || node2 != null) {
+            return false;
+        }
+
+        while (end1 != null && end2 != null) {
+            if (end1.value != end2.value) {
+                return false;
+            }
+            end1 = end1.pre;
+            end2 = end2.pre;
+        }
+
+        return null == end1 && null == end2;
+
     }
 
 
@@ -199,6 +290,13 @@ public class Code01_ReverseList {
         return pre;
     }
 
+    private static void printDoubleList(DoubleNode head) {
+        while (head != null) {
+            System.out.print(head.value + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
 
 
 }
