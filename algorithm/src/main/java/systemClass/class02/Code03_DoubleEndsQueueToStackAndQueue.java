9333e4b67154a9a51a8395f834321cfe81dac124
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-05-06 22:17:46 +0800

    2022.05.06：learning

---
+++ algorithm/src/main/java/systemClass/class02/Code03_DoubleEndsQueueToStackAndQueue.java
@@ -0,0 +1,8 @@
+package systemClass.class02;
+
+/**
+ * @author: thirteenmj
+ * @date: 2022-05-06 22:17
+ */
+public class Code03_DoubleEndsQueueToStackAndQueue {
+}
---
+++ algorithm/src/main/java/systemClass/class02/Code04_RingArray.java
@@ -0,0 +1,8 @@
+package systemClass.class02;
+
+/**
+ * @author: thirteenmj
+ * @date: 2022-05-06 22:16
+ */
+public class Code04_RingArray {
+}
