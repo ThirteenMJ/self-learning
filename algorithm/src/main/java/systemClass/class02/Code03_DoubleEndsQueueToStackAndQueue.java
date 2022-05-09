63d42474904099f5011b424ea36837eeb5d978fc
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-05-09 15:28:07 +0800

    2022.05.09：learning

--- algorithm/src/main/java/systemClass/class02/Code03_DoubleEndsQueueToStackAndQueue.java
+++ algorithm/src/main/java/systemClass/class02/Code03_DoubleEndsQueueToStackAndQueue.java
@@ -5,4 +5,31 @@ package systemClass.class02;
  * @date: 2022-05-06 22:17
  */
 public class Code03_DoubleEndsQueueToStackAndQueue {
+
+    public static class Node<V> {
+        public Node next;
+        public int val;
+
+        public Node() {
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(Node next, int val) {
+            this.next = next;
+            this.val = val;
+        }
+    }
+
+    public static class DoubleEndsQueue<V> {
+        public Node<V> head;
+        public Node<V> tail;
+
+        public DoubleEndsQueue(Node<V> head, Node<V> tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+    }
 }
