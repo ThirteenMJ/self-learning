a70ebb1ca64ee4dab508a6850567b57a51be77dd
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-05-11 16:43:25 +0800

    2022.05.11：learning

---
+++ algorithm/src/main/java/systemClass/class02/Code08_GetMax.java
@@ -0,0 +1,8 @@
+package systemClass.class02;
+
+/**
+ * @author: thirteenmj
+ * @date: 2022-05-11 16:11
+ */
+public class Code08_GetMax {
+}
---
+++ algorithm/src/main/java/systemClass/class03/Code04_BiggerThanRightTwice.java
@@ -0,0 +1,8 @@
+package systemClass.class03;
+
+/**
+ * @author: thirteenmj
+ * @date: 2022-05-11 16:12
+ */
+public class Code04_BiggerThanRightTwice {
+}
