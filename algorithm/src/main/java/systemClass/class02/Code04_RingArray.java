d3536fa03f8780c2374387f78401296799ffd8a6
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-06-12 16:46:12 +0800

    2022.06.12：learning

--- algorithm/src/main/java/systemClass/class02/Code04_RingArray.java
+++ algorithm/src/main/java/systemClass/class02/Code04_RingArray.java
@@ -1,15 +1,18 @@
 package systemClass.class02;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * 欠：对数器
+ * 环形数组实现队列
  *
  * @author: thirteenmj
  * @date: 2022-05-06 22:16
  */
 public class Code04_RingArray {
 
-    public class MyQueue {
+    public static class MyQueue {
         public int[] arr;
         public int size;
         public int head;
@@ -22,6 +25,10 @@ public class Code04_RingArray {
             this.tail = 0;
             this.size = 0;
             this.arr = new int[limit];
+        }
+
+        public int size() {
+            return this.size;
         }
 
         public void push(int value) {
@@ -50,5 +57,50 @@ public class Code04_RingArray {
         private int nextIndex(int tail) {
             return tail < limit - 1 ? tail + 1 : 0;
         }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 100000;
+        int maxValue = 10000;
+        for (int i = 0; i < testTime; i++) {
+            Queue<Integer> queue = new LinkedList<>();
+            int limit = (int) ((Math.random() * testTime) + 1);
+            MyQueue myQueue = new MyQueue(limit);
+            int operateTime = (int) (Math.random() * maxValue);
+            for (int j = 0; j < operateTime; j++) {
+                if (queue.isEmpty() != myQueue.isEmpty()) {
+                    System.out.println("判断是否为空结果不一致！");
+                    System.out.println("queue:" + queue.isEmpty());
+                    System.out.println("myQueue:" + myQueue.isEmpty());
+                    return;
+                }
+                if (queue.size() != myQueue.size) {
+                    System.out.println("数量结果不一致！");
+                    System.out.println("queue:" + queue.size());
+                    System.out.println("myQueue:" + myQueue.size());
+                    return;
+                }
+                int time = (int) Math.random();
+                if (time < 0.5) {
+                    if (myQueue.size() < limit) {
+                        int value = (int) (Math.random() * maxValue);
+                        myQueue.push(value);
+                        queue.add(value);
+                    }
+                } else {
+                    if (myQueue.size > 0) {
+                        int value1 = myQueue.pop();
+                        int value2 = queue.poll();
+                        if (value1 != value2) {
+                            System.out.println("弹出结果不一致");
+                            System.out.println("queue:" + value1);
+                            System.out.println("myQueue:" + value2);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("success");
     }
 }
