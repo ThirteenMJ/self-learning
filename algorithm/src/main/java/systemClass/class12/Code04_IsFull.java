6e2378a182a8a4745a7162b574a9f29adf0a46d7
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-09-22 21:27:24 +0800

    2022.09.22：判断树是否是满二叉树

--- algorithm/src/main/java/systemClass/class12/Code04_IsFull.java
+++ algorithm/src/main/java/systemClass/class12/Code04_IsFull.java
@@ -6,4 +6,45 @@ package systemClass.class12;
  * @date: 2022-09-21 20:10
  */
 public class Code04_IsFull {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static class Info2 {
+        public boolean isFull;
+        public int height;
+
+        public Info2() {
+        }
+
+        public Info2(boolean isFull, int height) {
+            this.isFull = isFull;
+            this.height = height;
+        }
+    }
+
+    public static boolean isFull(Node head) {
+        return process2(head).isFull;
+    }
+
+    public static Info2 process2(Node head) {
+        if (null == head) {
+            return new Info2(true, 0);
+        }
+
+        Info2 leftInfo = process2(head.left);
+        Info2 rightInfo = process2(head.right);
+
+        boolean isFull = leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height;
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        return new Info2(isFull, height);
+    }
+
 }
