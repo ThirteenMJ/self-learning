1b0941a77682fc8f80637a26e04b049edd0604d1
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-11-10 11:43:19 +0800

    2022.11.10：宽度优先级遍历

---
+++ algorithm/src/main/java/systemClass/class16/Code01_BFS.java
@@ -0,0 +1,38 @@
+package systemClass.class16;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * 宽度优先级遍历
+ * @author: thirteenmj
+ * @date: 2022-11-10 11:22
+ */
+public class Code01_BFS {
+
+    /**
+     * 从 start 出发，进行宽度有限遍历，并打印
+     * @param start
+     */
+    public static void bfs (Node start) {
+        if (null == start) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        Set<Node> set = new HashSet<>();
+        queue.add(start);
+        set.add(start);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    set.add(next);
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
---
+++ algorithm/src/main/java/systemClass/class16/Edge.java
@@ -0,0 +1,23 @@
+package systemClass.class16;
+
+/**
+ * @author: thirteenmj
+ * @date: 2022-11-10 11:26
+ */
+public class Edge {
+
+    public int weight;
+
+    public Node from;
+
+    public Node to;
+
+    public Edge() {
+    }
+
+    public Edge(int weight, Node from, Node to) {
+        this.weight = weight;
+        this.from = from;
+        this.to = to;
+    }
+}
