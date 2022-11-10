c6b1cf647d964d83ee676ceb4bcbe6ab0d336d7e
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-11-10 16:04:52 +0800

    2022.11.10：将无序的图变成有序集合

---
+++ algorithm/src/main/java/systemClass/class16/Code03_TopologicalOrderBFS.java
@@ -0,0 +1,52 @@
+package systemClass.class16;
+
+import java.util.*;
+
+/**
+ * 将无序的图变成有序集合
+ * OJ：https://www.lintcode.com/problem/topological-sorting
+ * @author: thirteenmj
+ * @date: 2022-11-10 15:47
+ */
+public class Code03_TopologicalOrderBFS {
+
+    public static class DirectedGraphNode {
+        public int label;
+        public ArrayList<DirectedGraphNode> neighbors;
+
+        public DirectedGraphNode(int x) {
+            label = x;
+            neighbors = new ArrayList<DirectedGraphNode>();
+        }
+    }
+
+    public static ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        HashMap<DirectedGraphNode, Integer> indegreeMap = new HashMap<>();
+        for (DirectedGraphNode cur : graph) {
+            indegreeMap.put(cur, 0);
+        }
+        for (DirectedGraphNode cur : graph) {
+            for (DirectedGraphNode next : cur.neighbors) {
+                indegreeMap.put(next, indegreeMap.get(next) + 1);
+            }
+        }
+        Queue<DirectedGraphNode> zeroQueue = new LinkedList<>();
+        for (DirectedGraphNode cur : indegreeMap.keySet()) {
+            if (indegreeMap.get(cur) == 0) {
+                zeroQueue.add(cur);
+            }
+        }
+        ArrayList<DirectedGraphNode> ans = new ArrayList<>();
+        while (!zeroQueue.isEmpty()) {
+            DirectedGraphNode cur = zeroQueue.poll();
+            ans.add(cur);
+            for (DirectedGraphNode next : cur.neighbors) {
+                indegreeMap.put(next, indegreeMap.get(next) - 1);
+                if (indegreeMap.get(next) == 0) {
+                    zeroQueue.add(next);
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ algorithm/src/main/java/systemClass/class16/Code04_Kruskal.java
@@ -0,0 +1,9 @@
+package systemClass.class16;
+
+/**
+ * @author: thirteenmj
+ * @date: 2022-11-10 14:28
+ */
+public class Code04_Kruskal {
+
+}
