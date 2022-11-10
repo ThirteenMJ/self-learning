ea5f880dbfc45ee911349fbde8bf7aa8bda86057
ThirteenMJ <75563066+ThirteenMJ@users.noreply.github.com>
2022-11-10 14:11:16 +0800

    2022.11.10：learning

---
+++ algorithm/src/main/java/systemClass/class16/Graph.java
@@ -0,0 +1,20 @@
+package systemClass.class16;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 图结构
+ * @author: thirteenmj
+ * @date: 2022-11-10 12:01
+ */
+public class Graph {
+    public HashMap<Integer, Node> nodes;
+
+    public HashSet<Edge> edges;
+
+    public Graph() {
+        nodes = new HashMap<>();
+        edges = new HashSet<>();
+    }
+}
